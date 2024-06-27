1b68216359fc365f3bc5680a288c94aaf640190e
Kiruthika <kiruthikauthayakumar25@gmail.com>
2024-06-27 15:12:47 +0530

    changes

--- filetracker/src/components/adminPage.js
+++ filetracker/src/components/adminPage.js
@@ -5,14 +5,12 @@ import '../adminPage.css';
 import { AuthContext } from './context';
 import Navbar from './navbar';
 
-
 export default function AdminPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
     const navigate = useNavigate();
-    const { setIsAdminLoggedIn,setDistrictId,isAdminLoggedIn} = useContext(AuthContext);
-
+    const { setIsAdminLoggedIn, setDistrictId, isAdminLoggedIn } = useContext(AuthContext);
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -24,11 +22,9 @@ export default function AdminPage() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
             const response = await axios.get(`http://localhost:8080/AdminLogin/${email}/${password}`);
-            if (response.data === true) 
-            {
+            if (response.data === true) {
                 const response2 = await axios.get(`http://localhost:8080/AdminLoginDetails/${email}/${password}`);
                 setDistrictId(response2.data.districtId);
                 setIsAdminLoggedIn(true);
@@ -36,8 +32,7 @@ export default function AdminPage() {
                 setTimeout(() => {
                     navigate("/adminWorking");
                 }, 0);
-            } else 
-            {
+            } else {
                 setMessage('Invalid email or password. Please try again.');
                 setTimeout(() => {
                     setMessage('');
@@ -49,28 +44,29 @@ export default function AdminPage() {
     };
 
     return (
-
         <div>
-            {!isAdminLoggedIn && <Navbar/>}
-        <div className="admin-container">
-            <h1 className="admin-title">Admin Login</h1>
-            <form onSubmit={handleSubmit}>
-                <label className="form-label">
-                    Email:
-                    <input className="form-input" type="email" value={email} onChange={handleEmailChange} />
-                </label>
-                <br />
-                <label className="form-label">
-                    Password:
-                    <input className="form-input" type="password" value={password} onChange={handlePasswordChange} />
-                </label>
-                <br />
-                <button className="submit-button" type="submit">Login</button>
-            </form>
-            {message && (
-                <div className="response-message">{message}</div>
-            )}
-        </div>
+            {!isAdminLoggedIn && <Navbar />}
+            <div className="admin-page-container">
+                <div className="admin-container">
+                    <h1 className="admin-title">Admin Login</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label className="form-label">
+                            Email:
+                            <input className="form-input" type="email" value={email} onChange={handleEmailChange} />
+                        </label>
+                        <br />
+                        <label className="form-label">
+                            Password:
+                            <input className="form-input" type="password" value={password} onChange={handlePasswordChange} />
+                        </label>
+                        <br />
+                        <button className="submit-button" type="submit">Login</button>
+                    </form>
+                    {message && (
+                        <div className="response-message">{message}</div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
